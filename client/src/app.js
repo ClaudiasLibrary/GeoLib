e9c115957cd439929d7402491af0ab430a7161e2
Claudia <168533353+ClaudiasLibrary@users.noreply.github.com>
2026-03-16 22:38:30 +0100

    Create main App component for GeoLib application

---
+++ client/src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./pages/Home";
+import MapPage from "./pages/MapPage";
+
+function App() {
+  return (
+    <div>
+      <h1>GeoLib</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
