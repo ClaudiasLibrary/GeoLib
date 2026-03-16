fbd847d6258776fd42905dcd1969ed7fcaac4ee0
Claudia <168533353+ClaudiasLibrary@users.noreply.github.com>
2026-03-16 22:47:04 +0100

    Add Home component with BookCard

---
+++ client/src/pages/home.js
@@ -0,0 +1,12 @@
+import BookCard from "../components/BookCard";
+
+function Home() {
+  return (
+    <div>
+      <h2>Book Collection</h2>
+      <BookCard title="Example Book" author="Author Name" />
+    </div>
+  );
+}
+
+export default Home;
